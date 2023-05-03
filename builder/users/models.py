4c8bfd97c6f50f692f43bd0af61ad76544ab3b52
ivanLFN <LFN.current@yandex.com>
2023-05-03 03:55:13 -0700

    Changed CustomerUser model.

--- builder/users/models.py
+++ builder/users/models.py
@@ -4,70 +4,6 @@ from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 
 
-class Departament(models.Model):
-
-    title = models.CharField(max_length=50, null=True, blank=True)
-
-    create_at = models.DateTimeField(auto_now_add=True)
-
-    description = models.TextField(null=True, blank=True)
-
-    def __str__(self) -> str:
-        return self.title
-
-
-class Skill(models.Model):
-
-    title = models.CharField(max_length=50, null=True, blank=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    description = models.TextField(null=True, blank=True)
-
-    def __str__(self) -> str:
-        return self.title
-
-
-class Position(models.Model):
-
-    title = models.CharField(max_length=50)
-
-    departament = models.ForeignKey(
-        Departament,
-        on_delete=models.CASCADE,
-        related_name='position_departament',
-        null=True, 
-        blank=True
-    )
-
-    base_salary = models.IntegerField()
-
-    description = models.TextField(default=None)
-
-    # Требования к кандидатам на позицию
-    requirements = models.TextField(default=None)
-
-    # Обязанности, которые необходимо выполнить на данной позиции
-    responsibilities = models.TextField(default=None)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    start_date = models.DateField()
-
-    end_date = models.DateField()
-
-    skills_required = models.ManyToManyField(
-        Skill,
-        related_name='position_skills_required'
-    )
-
-    experience_required = models.IntegerField()  # Требуемый опыт работы, в месяцах
-
-    is_active = models.BooleanField(default=False)
-
-    def __str__(self) -> str:
-        return self.title
-
 
 class Address(models.Model):
     country = models.CharField(max_length=255)
@@ -82,24 +18,6 @@ class Address(models.Model):
         return f"{self.country}, {self.region}, {self.city}, {self.street}, {self.house}"
 
 
-# Работает, уволен, в отпуске
-class State(models.Model):
-
-    title = models.CharField(max_length=255)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    start_date = models.DateField()
-
-    end_date = models.DateField()
-
-    is_active = models.BooleanField(default=False)
-
-    def __str__(self) -> str:
-        return self.title
-    
-
-
 class ClientGroup(models.Model):
     name = models.CharField(max_length=150, unique=True)
     permissions = models.ManyToManyField(
@@ -108,6 +26,7 @@ class ClientGroup(models.Model):
         help_text='The permissions this group has.',
         related_name='client_groups',
     )
+
 
 class ClientPermission(models.Model):
     name = models.CharField(max_length=255)
@@ -119,7 +38,6 @@ class ClientPermission(models.Model):
     codename = models.CharField(max_length=100)
 
 
-
 class CompanyGroup(models.Model):
     name = models.CharField(max_length=150, unique=True)
     permissions = models.ManyToManyField(
@@ -128,6 +46,7 @@ class CompanyGroup(models.Model):
         help_text='The permissions this group has.',
         related_name='company_groups',
     )
+
 
 class CompanyPermission(models.Model):
     name = models.CharField(max_length=255)
@@ -139,18 +58,12 @@ class CompanyPermission(models.Model):
     codename = models.CharField(max_length=100)
 
 
-
 class CustomUser(AbstractUser):
 
     avatar = models.ImageField(
         upload_to='avatars/',
         null=True,
         blank=True
-    )
-
-    positions = models.ManyToManyField(
-        Position,
-        related_name='user_positions'
     )
 
     rating = models.IntegerField(null=True, blank=True)
@@ -160,26 +73,9 @@ class CustomUser(AbstractUser):
     address = models.ForeignKey(
         Address,
         on_delete=models.CASCADE,
-        null=True, 
+        null=True,
         blank=True
     )
-
-    # dismissal_at = models.DateTimeField(default=None)
-
-    states = models.ManyToManyField(
-        State,
-        related_name='user_states'
-    )
-
-    # notes = models.ManyToManyField(Note)
-
-    skills = models.ManyToManyField(
-        Skill,
-        related_name='user_skills'
-    )
-
-
-
 
     # Не совсем понятен этот момент
     groups = models.ManyToManyField(
@@ -213,3 +109,168 @@ class Crew(models.Model):
 
     def __str__(self) -> str:
         return self.name_crew
+
+
+class TypeSkill(models.Model):
+
+    title_skill = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    description = models.TextField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self) -> str:
+        return self.title_skill
+
+
+class Skill(models.Model):
+
+    user_id = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='skill_user',
+        default=None,
+        blank=True
+    )
+
+    skill = models.ForeignKey(
+        TypeSkill,
+        on_delete=models.CASCADE,
+        related_name='skill_type',
+        default=None,
+        blank=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.created_at
+
+
+class TypeState(models.Model):
+
+    name_state = models.CharField(
+        max_length=50,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self) -> str:
+        return self.name_state
+
+
+# Работает, уволен, в отпуске
+class State(models.Model):
+
+    user_id = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='state_user',
+        default=None,
+        blank=True
+    )
+
+    type_state = models.ForeignKey(
+        TypeState,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='state_type'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    start_date = models.DateField()
+
+    end_date = models.DateField()
+
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.type_state
+    
+
+
+class Departament(models.Model):
+
+    title = models.CharField(max_length=50, null=True, blank=True)
+
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+
+class TypePosition(models.Model):
+
+    name_position = models.CharField(
+        max_length=50
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    base_salary = models.IntegerField()
+
+    # Требования к кандидатам на позицию
+    requirements = models.TextField(default=None)
+
+    # Обязанности, которые необходимо выполнить на данной позиции
+    responsibilities = models.TextField(default=None)
+
+    experience_required = models.IntegerField()  # Требуемый опыт работы, в месяцах
+
+    def __str__(self) -> str:
+        return self.name_position
+
+
+class Position(models.Model):
+
+    user_id = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='position_user',
+        default=None,
+        blank=True
+    )
+
+    type_position = models.ForeignKey(
+        TypePosition,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='position_type'
+    )
+
+    departament = models.ForeignKey(
+        Departament,
+        on_delete=models.CASCADE,
+        related_name='position_departament',
+        null=True,
+        blank=True
+    )
+
+    start_date = models.DateField()
+
+    end_date = models.DateField()
+
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.start_date
