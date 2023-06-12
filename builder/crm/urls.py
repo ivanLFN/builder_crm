8deebe9c433da1ea662ceaf0a0625002a0d3439e
ivanLFN <LFN.current@yandex.com>
2023-06-12 11:50:45 +0700

    removed main_menu files

--- builder/crm/urls.py
+++ builder/crm/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import include, path
-from crm.views import home_page
+from crm.views import home_page, clients_page, updates_page
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('home_page/', home_page, name='home_page'),
+    path('clients_page/', clients_page, name='clients_page'),
+    path('updates_page/', updates_page, name='updates_page'),
 ]
--- builder/crm/views.py
+++ builder/crm/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def home_page(request):
     pass
     return render(request, 'home_page/home_page.html', {})
+
+
+def clients_page(request):
+    pass
+    return render(request, 'clients_page/clients_page.html', {})
+
+
+def updates_page(request):
+    pass
+    return render(request, 'updates_page/updates_page.html', {})
