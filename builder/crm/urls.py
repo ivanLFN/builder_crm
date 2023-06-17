7d17b8746d7522712ba7b8e9520bdd683214ad6b
ivanLFN <LFN.current@yandex.com>
2023-06-17 11:46:09 +0700

    Added page for new order item. Added success modal window

--- builder/crm/urls.py
+++ builder/crm/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import include, path
-from crm.views import home_page, clients_page, updates_page, create_new_client
+from crm.views import home_page, clients_page, updates_page, create_new_client, create_new_order
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('clients_page/', clients_page, name='clients_page'),
     path('updates_page/', updates_page, name='updates_page'),
     path('create_new_client/', create_new_client, name='create_new_client'),
+    path('create_new_order/', create_new_order, name='create_new_order'),
 ]
